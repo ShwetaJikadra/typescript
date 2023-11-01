25e223100be4e58ede91801fea77d9642c7dec7d
ShwetaJikadra <setujikadra@gmail.com>
2023-11-01 16:35:13 +0530

    hello

---
+++ 1.ts
@@ -0,0 +1,34 @@
+// let x:string;
+// x="shweta jikadra"
+// console.log(x);
+
+// var a=(x1:number,y1:number)=>x1+y1;
+// console.log(a(10,20))
+
+// let y={a:1,b:2,c:3}
+// console.log(y)
+
+// var a1:[string?,number?]=["abcd",102]
+// console.log(a1)
+// a1.push(true)
+// a1.push("tt")
+// a1.push(12)
+// console.log(a1)
+
+// type obj={
+//     name:string;
+//     age?:number;
+//     email?:string
+// }
+
+// var a:obj={
+//     name:"jikadra",
+//     age:20
+// }
+// console.log(a)
+
+
+// let arr:[number?,string?,number?,string?]=[12,"shweta",13,"jikadra"]
+// console.log(arr)
+
+
